81e90fb0f9fc2cc7c895cac3cb60467606bcb750
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-19 13:04:34 +0200

    ADD tv show request and equal titles handling

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,34 @@ export default {
 </script>
 
 <template>
-  <input @keyup.enter="state.fetchMovie()" type="search" v-model.trim="state.searchText">
-  <button @click="state.fetchMovie()">Search</button>
-  <ul>
+  <input @keyup.enter="state.fetchMovies(), state.fetchTvShows()" type="search" v-model.trim="state.searchText">
+  <button @click="state.fetchMovies(), state.fetchTvShows()">Search</button>
+  <ul v-if="state.fetchedMovies.length > 0">
+    <li>
+      <h2>Movies: </h2>
+    </li>
     <li v-for="(movie, index) in state.fetchedMovies">
-      <h2>Movie {{ index }}</h2>
+      <h4>Movie {{ index }}</h4>
       <p>Title: {{ movie.title }}</p>
-      <p>Original title: {{ movie.original_title }}</p>
+      <p v-if="movie.title != movie.original_title">Original title: {{ movie.original_title }}</p>
       <p class="d-flex align-items-center">Language: 
         <img class="ms-2" :src="`https://flagcdn.com/32x24/${movie.original_language}.png`" :alt="`${movie.original_language} flag`">
       </p>
       <p>Score: {{ movie.vote_average }}</p>
+    </li>
+  </ul>
+  <ul v-if="state.fetchedTvShows.length > 0">
+    <li>
+      <h2>Tv Shows: </h2>
+    </li>
+    <li v-for="(tvShow, index) in state.fetchedTvShows">
+      <h4>Tv Show {{ index }}</h4>
+      <p>Title: {{ tvShow.name }}</p>
+      <p v-if="tvShow.name != tvShow.original_name">Original title: {{ tvShow.original_name }}</p>
+      <p class="d-flex align-items-center">Language: 
+        <img class="ms-2" :src="`https://flagcdn.com/32x24/${tvShow.original_language}.png`" :alt="`${tvShow.original_language} flag`">
+      </p>
+      <p>Score: {{ tvShow.vote_average }}</p>
     </li>
   </ul>
 </template>
--- src/state.js
+++ src/state.js
@@ -3,10 +3,12 @@ import axios from "axios"
 
 export const state = new reactive({
   searchMovieBaseApi: "https://api.themoviedb.org/3/search/movie",
+  searchTvShowBaseApi: "https://api.themoviedb.org/3/search/tv",
   apiKey: "47de7fcc53f94d3e0e183ea972c73ff3",
   searchText: "",
   fetchedMovies: [],
-  fetchMovie() {
+  fetchedTvShows: [],
+  fetchMovies() {
     let url = `${this.searchMovieBaseApi}?api_key=${this.apiKey}&query=${this.searchText.toLowerCase()}`;
     axios.get(url)
     .then(response => {
@@ -17,4 +19,15 @@ export const state = new reactive({
       console.error(error.message);
     })
   },
+  fetchTvShows() {
+    let url = `${this.searchTvShowBaseApi}?api_key=${this.apiKey}&query=${this.searchText.toLowerCase()}`;
+    axios.get(url)
+    .then(response => {
+      console.log(response.data.results);
+      this.fetchedTvShows = response.data.results;
+    })
+    .catch(error => {
+      console.error(error.message);
+    })
+  }
 })
