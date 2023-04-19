5fa29209e9c70630c860d59f42e153a8056cc6b8
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-19 12:03:14 +0200

    ADD search movie request

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script>
+import { state } from "./state.js"
 export default {
-
+  data() {
+    return {
+      state,
+    }
+  }
 }
 </script>
 
 <template>
-  <div>
-    
-  </div>
+  <input @keyup.enter="state.fetchMovie()" type="search" v-model.trim="state.searchText">
+  <button @click="state.fetchMovie()">Search</button>
+  <ul>
+    <li v-for="(movie, index) in state.fetchedMovies">
+      <h2>Movie {{ index }}</h2>
+      <p>Title: {{ movie.title }}</p>
+      <p>Original title: {{ movie.original_title }}</p>
+      <p>Language: {{ movie.original_language }}</p>
+      <p>Score: {{ movie.vote_average }}</p>
+    </li>
+  </ul>
 </template>
 
 <style>
--- src/state.js
+++ src/state.js
@@ -1,5 +1,20 @@
 import { reactive } from "vue"
+import axios from "axios"
 
 export const state = new reactive({
   searchMovieBaseApi: "https://api.themoviedb.org/3/search/movie",
+  apiKey: "47de7fcc53f94d3e0e183ea972c73ff3",
+  searchText: "",
+  fetchedMovies: [],
+  fetchMovie() {
+    let url = `${this.searchMovieBaseApi}?api_key=${this.apiKey}&query=${this.searchText.toLowerCase()}`;
+    axios.get(url)
+    .then(response => {
+      console.log(response.data.results);
+      this.fetchedMovies = response.data.results;
+    })
+    .catch(error => {
+      console.error(error.message);
+    })
+  },
 })
