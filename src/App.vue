0b8568c3536b6b26dfa622d03241e64e454d0b49
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-19 11:42:57 +0200

    ADD dependencies and basic assets

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 export default {
-  
+
 }
 </script>
 
 <template>
-
+  <div>
+    
+  </div>
 </template>
 
 <style>
---
+++ src/state.js
@@ -0,0 +1,5 @@
+import { reactive } from "vue"
+
+export const state = new reactive({
+  searchMovieBaseApi: "https://api.themoviedb.org/3/search/movie",
+})
