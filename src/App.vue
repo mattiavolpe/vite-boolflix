2c32f463d791fb444c64f92205b84aa98d1b8635
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-19 13:16:19 +0200

    CHANGE title and image order

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,11 @@ export default {
 <template>
   <input @keyup.enter="state.fetchMovies(), state.fetchTvShows()" type="search" v-model.trim="state.searchText">
   <button @click="state.fetchMovies(), state.fetchTvShows()">Search</button>
+  <h2 v-if="state.fetchedMovies.length > 0">Movies</h2>
   <ul v-if="state.fetchedMovies.length > 0">
-    <li>
-      <h2>Movies: </h2>
-    </li>
     <li v-for="(movie, index) in state.fetchedMovies">
-      <img v-if="movie.poster_path" :src="`${state.coverBaseUrl}${movie.poster_path}`" :alt="`${movie.title} cover image`">
       <h4>Movie {{ index }}</h4>
+      <img v-if="movie.poster_path" :src="`${state.coverBaseUrl}${movie.poster_path}`" :alt="`${movie.title} cover image`">
       <p>Title: {{ movie.title }}</p>
       <p v-if="movie.title != movie.original_title">Original title: {{ movie.original_title }}</p>
       <p class="d-flex align-items-center">Language: 
@@ -28,13 +26,11 @@ export default {
       <p>Score: {{ movie.vote_average }}</p>
     </li>
   </ul>
+  <h2 v-if="state.fetchedTvShows.length > 0">Tv Shows:</h2>
   <ul v-if="state.fetchedTvShows.length > 0">
-    <li>
-      <h2>Tv Shows: </h2>
-    </li>
     <li v-for="(tvShow, index) in state.fetchedTvShows">
-      <img v-if="tvShow.poster_path" :src="`${state.coverBaseUrl}${tvShow.poster_path}`" :alt="`${tvShow.title} cover image`">
       <h4>Tv Show {{ index }}</h4>
+      <img v-if="tvShow.poster_path" :src="`${state.coverBaseUrl}${tvShow.poster_path}`" :alt="`${tvShow.title} cover image`">
       <p>Title: {{ tvShow.name }}</p>
       <p v-if="tvShow.name != tvShow.original_name">Original title: {{ tvShow.original_name }}</p>
       <p class="d-flex align-items-center">Language: 
