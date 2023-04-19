c1730ed4e828e580a8a4ad96fb1fd064de999e57
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-19 12:53:16 +0200

    ADD temporary flag visualization

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script>
 import { state } from "./state.js"
+import axios from "axios"
 export default {
   data() {
     return {
       state,
     }
-  }
+  },
 }
 </script>
 
@@ -17,7 +18,9 @@ export default {
       <h2>Movie {{ index }}</h2>
       <p>Title: {{ movie.title }}</p>
       <p>Original title: {{ movie.original_title }}</p>
-      <p>Language: {{ movie.original_language }}</p>
+      <p class="d-flex align-items-center">Language: 
+        <img class="ms-2" :src="`https://flagcdn.com/32x24/${movie.original_language}.png`" :alt="`${movie.original_language} flag`">
+      </p>
       <p>Score: {{ movie.vote_average }}</p>
     </li>
   </ul>
