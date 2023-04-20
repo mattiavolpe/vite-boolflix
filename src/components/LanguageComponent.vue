7d0664c80b6e02012aac235c7b3a1a97c18ce842
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-04-20 11:29:35 +0200

    CHANGE languages behavior

--- src/components/LanguageComponent.vue
+++ src/components/LanguageComponent.vue
@@ -1,41 +1,115 @@
 <script>
+import arUrl from "./../assets/img/flags/ar.svg"
+import bnUrl from "./../assets/img/flags/bn.svg"
+import csUrl from "./../assets/img/flags/cs.svg"
+import deUrl from "./../assets/img/flags/de.svg"
+import enUrl from "./../assets/img/flags/en.svg"
+import esUrl from "./../assets/img/flags/es.svg"
+import faUrl from "./../assets/img/flags/fa.svg"
+import frUrl from "./../assets/img/flags/fr.svg"
+import haUrl from "./../assets/img/flags/ha.svg"
+import hiUrl from "./../assets/img/flags/hi.svg"
+import idUrl from "./../assets/img/flags/id.svg"
+import itUrl from "./../assets/img/flags/it.svg"
+import jaUrl from "./../assets/img/flags/ja.svg"
+import koUrl from "./../assets/img/flags/ko.svg"
+import nlUrl from "./../assets/img/flags/nl.svg"
+import plUrl from "./../assets/img/flags/pl.svg"
+import ptUrl from "./../assets/img/flags/pt.svg"
+import ruUrl from "./../assets/img/flags/ru.svg"
+import svUrl from "./../assets/img/flags/sv.svg"
+import trUrl from "./../assets/img/flags/tr.svg"
+import urUrl from "./../assets/img/flags/ur.svg"
+import viUrl from "./../assets/img/flags/vi.svg"
+import zhUrl from "./../assets/img/flags/zh.svg"
+import otherUrl from "./../assets/img/flags/other.jpg"
 export default {
   name: "LanguageComponent",
   props: {
     lang: String,
   },
+  data() {
+    return {
+      arUrl,
+      bnUrl,
+      csUrl,
+      deUrl,
+      enUrl,
+      esUrl,
+      faUrl,
+      frUrl,
+      haUrl,
+      hiUrl,
+      idUrl,
+      itUrl,
+      jaUrl,
+      koUrl,
+      nlUrl,
+      plUrl,
+      ptUrl,
+      ruUrl,
+      svUrl,
+      trUrl,
+      urUrl,
+      viUrl,
+      zhUrl,
+      otherUrl,
+    }
+  },
   computed: {
     checkLanguage() {
       switch (this.lang) {
-        case "aa":
-          return "et";
-        case "ak":
-          return "gh";
+        case "ar":
+          return arUrl;
         case "an":
-          return "es";
+        case "es":
+          return esUrl;
+        case "bn":
+          return bnUrl;
         case "ce":
-          return "ru";
+        case "ru":
+          return ruUrl;
         case "cs":
-          return "cz";
+          return csUrl;
+        case "de":
+          return deUrl;
         case "en":
-          return "gb";
+          return enUrl;
+        case "fa":
+          return faUrl;
+        case "fr":
+          return frUrl;
         case "ha":
-          return "ng";
-        case "ja":
-          return "jp";
-        case "ko":
-          return "kr";
-        case "ln":
-          return "cd";
+          return haUrl;
+        case "hi":
         case "ta":
-          return "in";
-        case "ti":
-          return "er";
-        // case "xx":
+          return hiUrl;
+        case "id":
+          return idUrl;
+        case "it":
+          return itUrl;
+        case "ja":
+          return jaUrl;
+        case "ko":
+          return koUrl;
+        case "nl":
+          return nlUrl;
+        case "pl":
+          return plUrl;
+        case "pt":
+          return ptUrl;
+        case "sv":
+          return svUrl;
+        case "tr":
+          return trUrl;
+        case "ur":
+          return urUrl;
+        case "vi":
+          return viUrl;
         case "zh":
-          return "cn";
+          return zhUrl;
         default:
-          return this.lang;
+          return otherUrl;
       }
     }
   }
@@ -43,7 +117,7 @@ export default {
 </script>
 
 <template>
-  <p class="d-flex align-items-center justify-content-center mb-2">Language:
-      <img class="ms-2" :src="`https://flagcdn.com/32x24/${checkLanguage}.png`" :alt="`${lang} flag`">
-    </p>
+  <p class="d-flex align-items-center justify-content-center mb-2">Language: {{ lang }}
+    <img width="32" height="24" class="ms-2" :src="checkLanguage" :alt="`${lang} flag`">
+  </p>
 </template>
